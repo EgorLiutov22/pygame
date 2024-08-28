c09030aae6c1677419acd0a92f2182da327b2b18
wayne <eliutov22@gmail.com>
2024-08-28 21:03:47 +0500

    ADD menu

--- example1.py
+++ example1.py
@@ -1,26 +1,97 @@
 import pygame
 
-# pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BG = 52, 78, 91
+
+font = pygame.font.SysFont('arialblack', 30)
+TEXT_COL = 255, 255, 255
+
+
+def draw_text(text, font, text_col, x, y, surface) -> None:
+    img = font.render(text, True, text_col)
+    surface.blit(img, (x, y))
+
+
+class Button:
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self, surface):
+        action = False
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+        return action
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Главное меню')
+resume_img = pygame.image.load('buttons/button_resume.png').convert_alpha()
+options_img = pygame.image.load('buttons/button_options.png').convert_alpha()
+quit_img = pygame.image.load('buttons/button_quit.png').convert_alpha()
+video_img = pygame.image.load('buttons/button_video.png').convert_alpha()
+audio_img = pygame.image.load('buttons/button_audio.png').convert_alpha()
+keys_img = pygame.image.load('buttons/button_keys.png').convert_alpha()
+back_img = pygame.image.load('buttons/button_back.png').convert_alpha()
+
+resume_button = Button(304, 125, resume_img, 1)
+options_button = Button(297, 250, options_img, 1)
+quit_button = Button(336, 375, quit_img, 1)
+video_button = Button(226, 75, video_img, 1)
+audio_button = Button(225, 200, audio_img, 1)
+keys_button = Button(246, 325, keys_img, 1)
+back_button = Button(332, 450, back_img, 1)
+
+screen.fill(BG)
+menu_state = 'main'
+game_pause = False
+run = True
+while run:
+    screen.fill(BG)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                game_pause = not game_pause
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    if game_pause:
+        if menu_state == 'main':
+            if resume_button.draw(screen):
+                game_pause = False
+            elif options_button.draw(screen):
+                menu_state = 'options'
+            elif quit_button.draw(screen):
+                run = False
+        elif menu_state == 'options':
+            if video_button.draw(screen):
+                print('Vidio options')
+            elif audio_button.draw(screen):
+                print('audio options')
+            elif keys_button.draw(screen):
+                print('Keys options')
+            elif back_button.draw(screen):
+                menu_state = 'main'
 
-    # RENDER YOUR GAME HERE
+    else:
+        draw_text("Нажмите пробел для паузы", font, TEXT_COL, 160, 250, screen)
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
+    pygame.display.update()
 
 pygame.quit()
